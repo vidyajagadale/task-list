e49a4dbc575f0eddee11672505507a27f0fa0539
Vidya Jagadale <vjagadale17@gmail.com>
2025-08-23 18:57:55 +0530

    Started working on task-list project for portfolio

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>No tasks found!</div>
+    @endforelse
+@endsection
