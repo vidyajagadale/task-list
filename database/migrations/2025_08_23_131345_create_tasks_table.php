03409e9933b86ff7bbe4007aa9fdbf2cda9a79c7
Vidya Jagadale <vjagadale17@gmail.com>
2025-08-23 19:10:19 +0530

    Task table created

--- database/migrations/2025_08_23_131345_create_tasks_table.php
+++ database/migrations/2025_08_23_131345_create_tasks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('long_description')->nullable();
+            $table->boolean('is_completed')->default(false);
             $table->timestamps();
         });
     }
